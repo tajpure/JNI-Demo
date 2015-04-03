ac60671d7b2fee6a806109e933058e5eec60fc7c
TAJPURE <tajpure@gmail.com>
2015-04-03 11:57:37 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,13 @@
 public class Hello {
 
-static {
-System.loadLibrary("Helloworld");
-}
-public native void displayHello();
-public static void main(String[] args){
-new Hello().displayHello();
-}
+  static {
+    System.loadLibrary("Helloworld");
+  }
+  
+  public native void displayHello();
+  
+  public static void main(String[] args){
+    new Hello().displayHello();
+  }
+  
 }
