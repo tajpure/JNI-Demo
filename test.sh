d49c70787e11e5eb77fa33b50a9274ad4a1309b1
TAJPURE <tajpure@gmail.com>
2015-04-03 11:59:18 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,3 @@
 # ln /user/shared/jdk1.7.0_75/include/linux/jni_md.h /user/shared/jdk1.7.0_75/include
 # g++ -shared -I /user/shared/jdk1.7.0_75/include jni_helloworldImpl.cpp -o Helloworld.so -fPIC
 java -Djava.library.path=. Hello
-
