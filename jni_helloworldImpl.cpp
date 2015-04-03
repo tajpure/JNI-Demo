c405e31439015b1cd54166939cfa8c6fec2ea3cc
TAJPURE <tajpure@gmail.com>
2015-04-03 12:39:28 +0800

    Update jni_helloworldImpl.cpp

--- jni_helloworldImpl.cpp
+++ jni_helloworldImpl.cpp
@@ -1,7 +1,8 @@
 #include<jni.h>
 #include "Hello.h"
 #include <stdio.h>
+
 JNIEXPORT void JNICALL Java_Hello_displayHello(JNIEnv *, jobject) {
-printf("Hello world!\n");
-return;
+  printf("Hello world!\n");
+  return;
 }
